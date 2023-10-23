98868cb0cd6bd67fbaa7d8431a6352bc74dec587
Жанна <nikochan884@gmail.com>
2023-10-23 15:30:06 +0700

    Interface

---
+++ ui_form.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'form.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QGroupBox, QLabel,
+    QListWidget, QListWidgetItem, QProgressBar, QPushButton,
+    QSizePolicy, QTabWidget, QWidget)
+
+class Ui_Widget(object):
+    def setupUi(self, Widget):
+        if not Widget.objectName():
+            Widget.setObjectName(u"Widget")
+        Widget.resize(800, 600)
+        Widget.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"background-color: rgb(212, 212, 212);")
+        self.DeleteButton = QPushButton(Widget)
+        self.DeleteButton.setObjectName(u"DeleteButton")
+        self.DeleteButton.setGeometry(QRect(10, 320, 151, 24))
+        self.DeleteButton.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"font: 700 10pt \"Perpetua Titling MT\";")
+        self.groupBox_2 = QGroupBox(Widget)
+        self.groupBox_2.setObjectName(u"groupBox_2")
+        self.groupBox_2.setGeometry(QRect(10, 360, 151, 71))
+        self.groupBox_2.setStyleSheet(u"background-color: rgb(231, 231, 231);\n"
+"\n"
+"font: 700 10pt \"Perpetua Titling MT\";")
+        self.checkBox = QCheckBox(self.groupBox_2)
+        self.checkBox.setObjectName(u"checkBox")
+        self.checkBox.setGeometry(QRect(10, 20, 78, 22))
+        self.checkBox_2 = QCheckBox(self.groupBox_2)
+        self.checkBox_2.setObjectName(u"checkBox_2")
+        self.checkBox_2.setGeometry(QRect(10, 40, 78, 22))
+        self.ProcessingButton = QPushButton(Widget)
+        self.ProcessingButton.setObjectName(u"ProcessingButton")
+        self.ProcessingButton.setGeometry(QRect(10, 440, 151, 24))
+        self.ProcessingButton.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"font: 700 10pt \"Perpetua Titling MT\";")
+        self.tabWidget = QTabWidget(Widget)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tabWidget.setGeometry(QRect(166, 40, 621, 551))
+        self.tabWidget.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"font: 700 10pt \"Perpetua Titling MT\";\n"
+"background-color: rgb(171, 171, 171);")
+        self.tab = QWidget()
+        self.tab.setObjectName(u"tab")
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName(u"tab_2")
+        self.tabWidget.addTab(self.tab_2, "")
+        self.progressBar = QProgressBar(Widget)
+        self.progressBar.setObjectName(u"progressBar")
+        self.progressBar.setGeometry(QRect(10, 480, 151, 23))
+        self.progressBar.setValue(24)
+        self.HelpHelp = QPushButton(Widget)
+        self.HelpHelp.setObjectName(u"HelpHelp")
+        self.HelpHelp.setGeometry(QRect(10, 550, 151, 24))
+        self.HelpHelp.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"font: 700 10pt \"Perpetua Titling MT\";")
+        self.SaveButton = QPushButton(Widget)
+        self.SaveButton.setObjectName(u"SaveButton")
+        self.SaveButton.setGeometry(QRect(10, 520, 151, 24))
+        self.SaveButton.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"font: 700 10pt \"Perpetua Titling MT\";")
+        self.listWidget = QListWidget(Widget)
+        self.listWidget.setObjectName(u"listWidget")
+        self.listWidget.setGeometry(QRect(10, 80, 151, 231))
+        self.listWidget.setStyleSheet(u"color: rgb(255, 255, 255);")
+        self.DownloadVideoButton = QPushButton(Widget)
+        self.DownloadVideoButton.setObjectName(u"DownloadVideoButton")
+        self.DownloadVideoButton.setGeometry(QRect(10, 40, 151, 24))
+        self.DownloadVideoButton.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"font: 700 10pt \"Perpetua Titling MT\";")
+        self.label = QLabel(Widget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(10, 10, 551, 16))
+        self.label.setStyleSheet(u"font: 600 12pt \"Bahnschrift SemiBold SemiConden\";")
+
+        self.retranslateUi(Widget)
+
+        self.tabWidget.setCurrentIndex(1)
+
+
+        QMetaObject.connectSlotsByName(Widget)
+    # setupUi
+
+    def retranslateUi(self, Widget):
+        Widget.setWindowTitle(QCoreApplication.translate("Widget", u"\u0421\u0438\u0431\u0438\u0440\u0441\u043a\u0438\u0439 \u0433\u043e\u0441\u0443\u0434\u0430\u0440\u0441\u0442\u0432\u0435\u043d\u043d\u044b\u0439 \u0443\u043d\u0438\u0432\u0435\u0440\u0441\u0438\u0442\u0435\u0442 \u0433\u0435\u043e\u0441\u0438\u0441\u0442\u0435\u043c \u0438 \u0442\u0435\u0445\u043d\u043e\u043b\u043e\u0433\u0438\u0439 ", None))
+        self.DeleteButton.setText(QCoreApplication.translate("Widget", u"\u0423\u0434\u0430\u043b\u0438\u0442\u044c \u0444\u0430\u0439\u043b", None))
+        self.groupBox_2.setTitle(QCoreApplication.translate("Widget", u"\u041e\u0431\u0440\u0430\u0431\u043e\u0442\u0430\u0442\u044c", None))
+        self.checkBox.setText(QCoreApplication.translate("Widget", u"\u0424\u043e\u0442\u043e", None))
+        self.checkBox_2.setText(QCoreApplication.translate("Widget", u"\u0412\u0438\u0434\u0435\u043e", None))
+        self.ProcessingButton.setText(QCoreApplication.translate("Widget", u"\u041e\u0431\u0440\u0430\u0431\u043e\u0442\u0430\u0442\u044c", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("Widget", u"\u0424\u0430\u0439\u043b \u0434\u043e \u043e\u0431\u0440\u0430\u0431\u043e\u0442\u043a\u0438", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("Widget", u"\u0424\u0430\u0439\u043b \u043f\u043e\u0441\u043b\u0435 \u043e\u0431\u0440\u0430\u0431\u043e\u0442\u043a\u0438", None))
+        self.HelpHelp.setText(QCoreApplication.translate("Widget", u"\u0421\u043f\u0440\u0430\u0432\u043a\u0430", None))
+        self.SaveButton.setText(QCoreApplication.translate("Widget", u"\u0421\u043e\u0445\u0440\u0430\u043d\u0438\u0442\u044c", None))
+        self.DownloadVideoButton.setText(QCoreApplication.translate("Widget", u"\u0417\u0430\u0433\u0440\u0443\u0437\u0438\u0442\u044c \u0444\u0430\u0439\u043b", None))
+        self.label.setText(QCoreApplication.translate("Widget", u"\u0410\u043f\u043f\u0430\u0440\u0430\u0442\u043d\u043e-\u043f\u0440\u043e\u0433\u0440\u0430\u043c\u043c\u043d\u044b\u0439 \u043a\u043e\u043c\u043f\u043b\u0435\u043a\u0441 \u043e\u043f\u0440\u0435\u0434\u0435\u043b\u0435\u043d\u0438\u044f \u0434\u0435\u0444\u043e\u0440\u043c\u0430\u0446\u0438\u0439 \u0437\u0434\u0430\u043d\u0438\u0439 \u0438 \u0441\u043e\u043e\u0440\u0443\u0436\u0435\u043d\u0438\u0439 ", None))
+    # retranslateUi
+
---
+++ widget.py
@@ -0,0 +1,57 @@
+# This Python file uses the following encoding: utf-8
+import sys
+import os
+
+from PySide6 import QtWidgets
+from PySide6.QtWidgets import *
+
+# Important:
+# You need to run the following command to generate the ui_form.py file
+#     pyside6-uic form.ui -o ui_form.py, or
+#     pyside2-uic form.ui -o ui_form.py
+from ui_form import Ui_Widget
+
+
+class Widget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_Widget()
+        self.ui.setupUi(self)
+        self.ui.DownloadVideoButton.clicked.connect(self.getMaterialVP)
+        self.ui.DeleteButton.clicked.connect(self.deleteSelectedItem)
+        self.ui.HelpHelp.clicked.connect(self.showHelp)
+        # loader = QUiLoader()
+        # ui_file = QFile("form.ui")
+        # ui_file.open(QFile.ReadOnly)
+        # self.ui = loader.load(ui_file)
+
+        # ui_file.close()
+
+    def getMaterialVP(self):
+        directory = QtWidgets.QFileDialog.getOpenFileNames(self, filter="AVI JPG JPEG MP4 PNG (*.avi *.jpg *.jpeg "
+                                                                        "*.mp4 *.png) ;; MP4 (*.mp4);; PNG (*.png);; "
+                                                                        "JPEG (*.jpeg);; JPG (*.jpg)")
+        print(directory)
+
+        for obj in directory[0]:
+            self.ui.listWidget.addItem(os.path.basename(obj))
+
+    def deleteSelectedItem(self):
+        selectitem = self.ui.listWidget.currentItem()
+        if selectitem:
+            self.ui.listWidget.takeItem(self.ui.listWidget.row(selectitem))
+
+    def showHelp(self):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setText("You look pretty")
+        msg.setDetailedText("Здесь будет отображаться подсказка для пользователя.")
+        msg.setWindowTitle("Помощь")
+        msg.exec_()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    widget = Widget()
+    widget.show()
+    sys.exit(app.exec())
