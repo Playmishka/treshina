330536b7d02de4efe5f345b0c130d7c00a0ec6ed
Playmishka <mrcraftermiha@gmail.com>
2023-10-28 16:46:49 +0700

    New Form

--- ui_form.py
+++ ui_form.py
@@ -9,136 +9,127 @@
 ################################################################################
 
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-							QMetaObject, QObject, QPoint, QRect,
-							QSize, QTime, QUrl, Qt)
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
 from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-						   QFont, QFontDatabase, QGradient, QIcon,
-						   QImage, QKeySequence, QLinearGradient, QPainter,
-						   QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGroupBox, QLabel, QListWidget,
-							   QListWidgetItem, QProgressBar, QPushButton, QRadioButton,
-							   QSizePolicy, QTabWidget, QWidget)
-
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDoubleSpinBox, QGroupBox, QLabel,
+    QListWidget, QListWidgetItem, QPushButton, QRadioButton,
+    QSizePolicy, QTabWidget, QWidget)
 
 class Ui_Widget(object):
-	def setupUi(self, Widget):
-		if not Widget.objectName():
-			Widget.setObjectName(u"Widget")
-		Widget.resize(800, 600)
-		Widget.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
-							 "background-color: rgb(212, 212, 212);")
-		self.DeleteButton = QPushButton(Widget)
-		self.DeleteButton.setObjectName(u"DeleteButton")
-		self.DeleteButton.setGeometry(QRect(10, 360, 171, 24))
-		self.DeleteButton.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
-										"font: 700 10pt \"Perpetua Titling MT\";\n"
-										"border-radius: 7px;\n"
-										"")
-		self.groupBox_2 = QGroupBox(Widget)
-		self.groupBox_2.setObjectName(u"groupBox_2")
-		self.groupBox_2.setGeometry(QRect(10, 390, 171, 71))
-		self.groupBox_2.setStyleSheet(u"background-color: rgb(231, 231, 231);\n"
-									  "border-radius: 7px;\n"
-									  "font: 700 10pt \"Perpetua Titling MT\";")
-		self.radioButton = QRadioButton(self.groupBox_2)
-		self.radioButton.setObjectName(u"radioButton")
-		self.radioButton.setGeometry(QRect(10, 20, 89, 20))
-		self.radioButton_2 = QRadioButton(self.groupBox_2)
-		self.radioButton_2.setObjectName(u"radioButton_2")
-		self.radioButton_2.setGeometry(QRect(10, 40, 89, 20))
-		self.ProcessingButton = QPushButton(Widget)
-		self.ProcessingButton.setObjectName(u"ProcessingButton")
-		self.ProcessingButton.setGeometry(QRect(10, 470, 171, 24))
-		self.ProcessingButton.setStyleSheet(u"border-radius: 7px;\n"
-											"background-color: rgb(255, 255, 255);\n"
-											"font: 700 10pt \"Perpetua Titling MT\";")
-		self.tabWidget = QTabWidget(Widget)
-		self.tabWidget.setObjectName(u"tabWidget")
-		self.tabWidget.setGeometry(QRect(190, 40, 601, 551))
-		self.tabWidget.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
-									 "font: 700 10pt \"Perpetua Titling MT\";\n"
-									 "background-color: rgb(194, 194, 194);\n"
-									 "border-radius: 10px;")
-		self.tab = QWidget()
-		self.tab.setObjectName(u"tab")
-		self.tabWidget.addTab(self.tab, "")
-		self.tab_2 = QWidget()
-		self.tab_2.setObjectName(u"tab_2")
-		self.tabWidget.addTab(self.tab_2, "")
-		self.progressBar = QProgressBar(Widget)
-		self.progressBar.setObjectName(u"progressBar")
-		self.progressBar.setGeometry(QRect(10, 500, 171, 23))
-		self.progressBar.setStyleSheet(u"")
-		self.progressBar.setValue(24)
-		self.HelpHelp = QPushButton(Widget)
-		self.HelpHelp.setObjectName(u"HelpHelp")
-		self.HelpHelp.setGeometry(QRect(10, 560, 171, 24))
-		self.HelpHelp.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
-									"font: 700 10pt \"Perpetua Titling MT\";\n"
-									"border-radius: 7px;\n"
-									"\n"
-									"")
-		self.SaveButton = QPushButton(Widget)
-		self.SaveButton.setObjectName(u"SaveButton")
-		self.SaveButton.setGeometry(QRect(10, 530, 171, 24))
-		self.SaveButton.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
-									  "font: 700 10pt \"Perpetua Titling MT\";\n"
-									  "border-radius: 7px;\n"
-									  "")
-		self.listWidget = QListWidget(Widget)
-		self.listWidget.setObjectName(u"listWidget")
-		self.listWidget.setGeometry(QRect(10, 70, 171, 281))
-		self.listWidget.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
-									  "font: 9pt \"PT Bold Heading\";\n"
-									  "border-radius: 3px;")
-		self.DownloadVideoButton = QPushButton(Widget)
-		self.DownloadVideoButton.setObjectName(u"DownloadVideoButton")
-		self.DownloadVideoButton.setGeometry(QRect(10, 40, 171, 24))
-		self.DownloadVideoButton.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
-											   "font: 700 10pt \"Perpetua Titling MT\";\n"
-											   "border-radius: 7px;")
-		self.label = QLabel(Widget)
-		self.label.setObjectName(u"label")
-		self.label.setGeometry(QRect(10, 10, 501, 16))
-		self.label.setStyleSheet(u"font: 700 10pt \"Perpetua Titling MT\";\n"
-								 "font-weight: bold;\n"
-								 "Front-size: 40pt;\n"
-								 "")
+    def setupUi(self, Widget):
+        if not Widget.objectName():
+            Widget.setObjectName(u"Widget")
+        Widget.resize(800, 600)
+        Widget.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"background-color: rgb(212, 212, 212);")
+        self.DeleteButton = QPushButton(Widget)
+        self.DeleteButton.setObjectName(u"DeleteButton")
+        self.DeleteButton.setGeometry(QRect(10, 340, 171, 24))
+        self.DeleteButton.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"font: 700 10pt \"Perpetua Titling MT\";\n"
+"border-radius: 7px;\n"
+"")
+        self.groupBox_2 = QGroupBox(Widget)
+        self.groupBox_2.setObjectName(u"groupBox_2")
+        self.groupBox_2.setGeometry(QRect(10, 370, 171, 61))
+        self.groupBox_2.setStyleSheet(u"background-color: rgb(231, 231, 231);\n"
+"border-radius: 7px;\n"
+"font: 700 10pt \"Perpetua Titling MT\";")
+        self.radioButton = QRadioButton(self.groupBox_2)
+        self.radioButton.setObjectName(u"radioButton")
+        self.radioButton.setGeometry(QRect(10, 20, 89, 20))
+        self.radioButton_2 = QRadioButton(self.groupBox_2)
+        self.radioButton_2.setObjectName(u"radioButton_2")
+        self.radioButton_2.setGeometry(QRect(10, 40, 89, 20))
+        self.ProcessingButton = QPushButton(Widget)
+        self.ProcessingButton.setObjectName(u"ProcessingButton")
+        self.ProcessingButton.setGeometry(QRect(10, 510, 171, 24))
+        self.ProcessingButton.setStyleSheet(u"border-radius: 7px;\n"
+"background-color: rgb(255, 255, 255);\n"
+"font: 700 10pt \"Perpetua Titling MT\";")
+        self.tabWidget = QTabWidget(Widget)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tabWidget.setGeometry(QRect(190, 40, 601, 551))
+        self.tabWidget.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"font: 700 10pt \"Perpetua Titling MT\";\n"
+"background-color: rgb(194, 194, 194);\n"
+"border-radius: 10px;")
+        self.tab = QWidget()
+        self.tab.setObjectName(u"tab")
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName(u"tab_2")
+        self.tabWidget.addTab(self.tab_2, "")
+        self.HelpHelp = QPushButton(Widget)
+        self.HelpHelp.setObjectName(u"HelpHelp")
+        self.HelpHelp.setGeometry(QRect(10, 570, 171, 24))
+        self.HelpHelp.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"font: 700 10pt \"Perpetua Titling MT\";\n"
+"border-radius: 7px;\n"
+"\n"
+"")
+        self.SaveButton = QPushButton(Widget)
+        self.SaveButton.setObjectName(u"SaveButton")
+        self.SaveButton.setGeometry(QRect(10, 540, 171, 24))
+        self.SaveButton.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"font: 700 10pt \"Perpetua Titling MT\";\n"
+"border-radius: 7px;\n"
+"")
+        self.listWidget = QListWidget(Widget)
+        self.listWidget.setObjectName(u"listWidget")
+        self.listWidget.setGeometry(QRect(10, 70, 171, 261))
+        self.listWidget.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"font: 9pt \"PT Bold Heading\";\n"
+"border-radius: 3px;")
+        self.DownloadVideoButton = QPushButton(Widget)
+        self.DownloadVideoButton.setObjectName(u"DownloadVideoButton")
+        self.DownloadVideoButton.setGeometry(QRect(10, 40, 171, 24))
+        self.DownloadVideoButton.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"font: 700 10pt \"Perpetua Titling MT\";\n"
+"border-radius: 7px;")
+        self.label = QLabel(Widget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(10, 10, 501, 16))
+        self.label.setStyleSheet(u"font: 700 10pt \"Perpetua Titling MT\";\n"
+"font-weight: bold;\n"
+"Front-size: 40pt;\n"
+"")
+        self.doubleSpinBox = QDoubleSpinBox(Widget)
+        self.doubleSpinBox.setObjectName(u"doubleSpinBox")
+        self.doubleSpinBox.setGeometry(QRect(10, 480, 171, 25))
+        self.label_2 = QLabel(Widget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(10, 440, 171, 31))
+        self.label_2.setStyleSheet(u"background-color: rgb(194, 194, 194);\n"
+"font: 700 10pt \"Perpetua Titling MT\";\n"
+"border-radius: 7px;\n"
+"")
 
-		self.retranslateUi(Widget)
+        self.retranslateUi(Widget)
 
-		self.tabWidget.setCurrentIndex(1)
+        self.tabWidget.setCurrentIndex(1)
 
-		QMetaObject.connectSlotsByName(Widget)
 
-	# setupUi
+        QMetaObject.connectSlotsByName(Widget)
+    # setupUi
 
-	def retranslateUi(self, Widget):
-		Widget.setWindowTitle(QCoreApplication.translate("Widget",
-														 u"\u0421\u0438\u0431\u0438\u0440\u0441\u043a\u0438\u0439 \u0433\u043e\u0441\u0443\u0434\u0430\u0440\u0441\u0442\u0432\u0435\u043d\u043d\u044b\u0439 \u0443\u043d\u0438\u0432\u0435\u0440\u0441\u0438\u0442\u0435\u0442 \u0433\u0435\u043e\u0441\u0438\u0441\u0442\u0435\u043c \u0438 \u0442\u0435\u0445\u043d\u043e\u043b\u043e\u0433\u0438\u0439 ",
-														 None))
-		self.DeleteButton.setText(
-			QCoreApplication.translate("Widget", u"\u0423\u0434\u0430\u043b\u0438\u0442\u044c \u0444\u0430\u0439\u043b",
-									   None))
-		self.groupBox_2.setTitle(
-			QCoreApplication.translate("Widget", u"\u041e\u0431\u0440\u0430\u0431\u043e\u0442\u0430\u0442\u044c", None))
-		self.radioButton.setText(QCoreApplication.translate("Widget", u"\u0424\u043e\u0442\u043e ", None))
-		self.radioButton_2.setText(QCoreApplication.translate("Widget", u"\u0412\u0438\u0434\u0435\u043e", None))
-		self.ProcessingButton.setText(
-			QCoreApplication.translate("Widget", u"\u041e\u0431\u0440\u0430\u0431\u043e\u0442\u0430\u0442\u044c", None))
-		self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("Widget",
-																							   u"\u0424\u0430\u0439\u043b \u0434\u043e \u043e\u0431\u0440\u0430\u0431\u043e\u0442\u043a\u0438",
-																							   None))
-		self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("Widget",
-																								 u"\u0424\u0430\u0439\u043b \u043f\u043e\u0441\u043b\u0435 \u043e\u0431\u0440\u0430\u0431\u043e\u0442\u043a\u0438",
-																								 None))
-		self.HelpHelp.setText(QCoreApplication.translate("Widget", u"\u0421\u043f\u0440\u0430\u0432\u043a\u0430", None))
-		self.SaveButton.setText(
-			QCoreApplication.translate("Widget", u"\u0421\u043e\u0445\u0440\u0430\u043d\u0438\u0442\u044c", None))
-		self.DownloadVideoButton.setText(QCoreApplication.translate("Widget",
-																	u"\u0417\u0430\u0433\u0440\u0443\u0437\u0438\u0442\u044c \u0444\u0430\u0439\u043b",
-																	None))
-		self.label.setText(QCoreApplication.translate("Widget",
-													  u"\u041f\u0440\u043e\u0433\u0440\u0430\u043c\u043c\u043d\u044b\u0439 \u043a\u043e\u043c\u043f\u043b\u0435\u043a\u0441 \u043e\u043f\u0440\u0435\u0434\u0435\u043b\u0435\u043d\u0438\u044f \u0434\u0435\u0444\u043e\u0440\u043c\u0430\u0446\u0438\u0439 \u0437\u0434\u0430\u043d\u0438\u0439 \u0438 \u0441\u043e\u043e\u0440\u0443\u0436\u0435\u043d\u0438\u0439 ",
-													  None))
-# retranslateUi
+    def retranslateUi(self, Widget):
+        Widget.setWindowTitle(QCoreApplication.translate("Widget", u"\u0421\u0438\u0431\u0438\u0440\u0441\u043a\u0438\u0439 \u0433\u043e\u0441\u0443\u0434\u0430\u0440\u0441\u0442\u0432\u0435\u043d\u043d\u044b\u0439 \u0443\u043d\u0438\u0432\u0435\u0440\u0441\u0438\u0442\u0435\u0442 \u0433\u0435\u043e\u0441\u0438\u0441\u0442\u0435\u043c \u0438 \u0442\u0435\u0445\u043d\u043e\u043b\u043e\u0433\u0438\u0439 ", None))
+        self.DeleteButton.setText(QCoreApplication.translate("Widget", u"\u0423\u0434\u0430\u043b\u0438\u0442\u044c \u0444\u0430\u0439\u043b", None))
+        self.groupBox_2.setTitle(QCoreApplication.translate("Widget", u"\u041e\u0431\u0440\u0430\u0431\u043e\u0442\u0430\u0442\u044c", None))
+        self.radioButton.setText(QCoreApplication.translate("Widget", u"\u0424\u043e\u0442\u043e ", None))
+        self.radioButton_2.setText(QCoreApplication.translate("Widget", u"\u0412\u0438\u0434\u0435\u043e", None))
+        self.ProcessingButton.setText(QCoreApplication.translate("Widget", u"\u041e\u0431\u0440\u0430\u0431\u043e\u0442\u0430\u0442\u044c", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("Widget", u"\u0424\u0430\u0439\u043b \u0434\u043e \u043e\u0431\u0440\u0430\u0431\u043e\u0442\u043a\u0438", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("Widget", u"\u0424\u0430\u0439\u043b \u043f\u043e\u0441\u043b\u0435 \u043e\u0431\u0440\u0430\u0431\u043e\u0442\u043a\u0438", None))
+        self.HelpHelp.setText(QCoreApplication.translate("Widget", u"\u0421\u043f\u0440\u0430\u0432\u043a\u0430", None))
+        self.SaveButton.setText(QCoreApplication.translate("Widget", u"\u0421\u043e\u0445\u0440\u0430\u043d\u0438\u0442\u044c", None))
+        self.DownloadVideoButton.setText(QCoreApplication.translate("Widget", u"\u0417\u0430\u0433\u0440\u0443\u0437\u0438\u0442\u044c \u0444\u0430\u0439\u043b", None))
+        self.label.setText(QCoreApplication.translate("Widget", u"\u041f\u0440\u043e\u0433\u0440\u0430\u043c\u043c\u043d\u044b\u0439 \u043a\u043e\u043c\u043f\u043b\u0435\u043a\u0441 \u043e\u043f\u0440\u0435\u0434\u0435\u043b\u0435\u043d\u0438\u044f \u0434\u0435\u0444\u043e\u0440\u043c\u0430\u0446\u0438\u0439 \u0437\u0434\u0430\u043d\u0438\u0439 \u0438 \u0441\u043e\u043e\u0440\u0443\u0436\u0435\u043d\u0438\u0439 ", None))
+        self.label_2.setText(QCoreApplication.translate("Widget", u"\u041a\u043e\u044d\u0444\u0444\u0438\u0446\u0438\u0435\u043d\u0442 \u043e\u0431\u0440\u0430\u0431\u043e\u0442\u043a\u0438:", None))
+    # retranslateUi
+
