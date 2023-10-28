bbfa843fca88bd0bea5eef16a2c2cf6513cd7542
Playmishka <mrcraftermiha@gmail.com>
2023-10-28 16:38:36 +0700

    Add file requirements for easy installing libraries

--- widget.py
+++ widget.py
@@ -18,80 +18,91 @@ from ui_form import Ui_Widget
 
 
 class Widget(QWidget):
-    listPath: list = []
-    model = YOLO("model/best.pt")
+	listPath: list = []
+	model = YOLO("model/best.pt")
 
-    # Конструктор класса окна приложения.
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.ui = Ui_Widget()
-        self.ui.setupUi(self)
-        self.ui.DownloadVideoButton.clicked.connect(self.getMaterialVP)
-        self.ui.DeleteButton.clicked.connect(self.deleteSelectedItem)
-        self.ui.HelpHelp.clicked.connect(self.showHelp)
-        self.ui.listWidget.itemDoubleClicked.connect(self.ViewImage)
-        self.ui.ProcessingButton.clicked.connect(self.process)
+	# Конструктор класса окна приложения.
+	def __init__(self, parent=None):
+		super().__init__(parent)
+		self.ui = Ui_Widget()
+		self.ui.setupUi(self)
+		self.ui.DownloadVideoButton.clicked.connect(self.getMaterialVP)
+		self.ui.DeleteButton.clicked.connect(self.deleteSelectedItem)
+		self.ui.HelpHelp.clicked.connect(self.showHelp)
+		self.ui.listWidget.itemDoubleClicked.connect(self.ViewImage)
+		self.ui.ProcessingButton.clicked.connect(self.process)
 
-        # loader = QUiLoader()
-        # ui_file = QFile("form.ui")
-        # ui_file.open(QFile.ReadOnly)
-        # self.ui = loader.load(ui_file)
-        # ui_file.close()
+	# loader = QUiLoader()
+	# ui_file = QFile("form.ui")
+	# ui_file.open(QFile.ReadOnly)
+	# self.ui = loader.load(ui_file)
+	# ui_file.close()
 
-    # Получение материалов для обработки.
-    def getMaterialVP(self):
-        directory = QtWidgets.QFileDialog.getOpenFileNames(self, filter="AVI JPG JPEG MP4 PNG (*.avi *.jpg *.jpeg "
-                                                                        "*.mp4 *.png) ;; AVI (*.avi) ;; MP4 (*.mp4);; PNG (*.png);; "
-                                                                        "JPEG (*.jpeg);; JPG (*.jpg)")
+	# Получение материалов для обработки.
+	def getMaterialVP(self):
+		directory = QtWidgets.QFileDialog.getOpenFileNames(self, filter="AVI JPG JPEG MP4 PNG (*.avi *.jpg *.jpeg "
+																		"*.mp4 *.png) ;; AVI (*.avi) ;; MP4 (*.mp4);;"
+																		"PNG (*.png);; JPEG (*.jpeg);; JPG (*.jpg)")
 
-        # for obj in directory[0]:
-        #     self.ui.listWidget.addItem(os.path.basename(obj))
-        #     self.listPath.append(directory[0])
-        for obj in directory[0]:
-            self.ui.listWidget.addItem(obj)
-            self.listPath.append(obj)
+		# for obj in directory[0]:
+		#     self.ui.listWidget.addItem(os.path.basename(obj))
+		#     self.listPath.append(directory[0])
+		for obj in directory[0]:
+			self.ui.listWidget.addItem(obj)
+			self.listPath.append(obj)
 
-    # Удаление объектов.
+	# Удаление объектов.
 
-    def deleteSelectedItem(self):
-        selectitem = self.ui.listWidget.currentItem()
-        if selectitem:
-            self.ui.listWidget.takeItem(self.ui.listWidget.row(selectitem))
+	def deleteSelectedItem(self):
+		selectitem = self.ui.listWidget.currentItem()
+		print(self.listPath)
+		if selectitem:
+			self.ui.listWidget.takeItem(self.ui.listWidget.row(selectitem))
+			print("Файл удален!")
+			self.listPath.remove(selectitem.text())
+		print(self.listPath)
 
-    # Получение доп. информации.
-    def showHelp(self):
-        msg = QMessageBox()
-        msg.setIcon(QMessageBox.Information)
-        msg.setText("You look pretty")
-        msg.setDetailedText(
-            "Здесь будет отображаться подсказка для пользователя.")
-        msg.setWindowTitle("Помощь")
-        msg.exec_()
+	# Получение доп. информации.
+	def showHelp(self):
+		msg = QMessageBox()
+		msg.setIcon(QMessageBox.Icon.Information)
+		msg.setText("You look pretty")
+		msg.setDetailedText(
+			"Здесь будет отображаться подсказка для пользователя.")
+		msg.setWindowTitle("Помощь")
+		msg.exec_()
 
-    # Отображение изображений
-    def ViewImage(self):
-        selected_item = self.ui.listWidget.currentItem()
-        if selected_item:
-            # Загружаем изображение из пути, хранящегося в тексте элемента
-            pixmap = QPixmap(selected_item.text())
-            self.ui.label_2.setFixedSize(300, 300)
-            label_2_width = self.ui.label_2.width()  # Получаем ширину label_2
-            label_2_height = self.ui.label_2.height()  # Получаем высоту label_2
-            # Масштабируем изображение
-            pixmap = pixmap.scaled(
-                label_2_width, label_2_height, aspectMode=Qt.KeepAspectRatio)
-            # В этой строке мы устанавливаем загруженное изображение (pixmap) в label_2
-            self.ui.label_2.setPixmap(pixmap)
+	# Отображение изображений
+	def ViewImage(self):
+		selected_item = self.ui.listWidget.currentItem()
+		if selected_item:
+			# Загружаем изображение из пути, хранящегося в тексте элемента
+			pixmap = QPixmap(selected_item.text())
+			self.ui.label_2.setFixedSize(300, 300)
+			label_2_width = self.ui.label_2.width()  # Получаем ширину label_2
+			label_2_height = self.ui.label_2.height()  # Получаем высоту label_2
+			# Масштабируем изображение
+			pixmap = pixmap.scaled(
+				label_2_width, label_2_height, aspectMode=Qt.KeepAspectRatio)
+			# В этой строке мы устанавливаем загруженное изображение (pixmap) в label_2
+			self.ui.label_2.setPixmap(pixmap)
 
-    def process(self):
-        self.model.predict(source=self.listPath, save=True, conf=0.1)
-        # print(settings)
-        print("Обработка завершена. Посмотри результаты в папке runs :)")
+	def process(self):
+		print(len(self.listPath))
+		if len(self.listPath) == 0:
+			msg = QMessageBox()
+			msg.setIcon(QMessageBox.Icon.Critical)
+			msg.setText("Отсутствуют файлы для обработки")
+			msg.setWindowTitle("Ошибка")
+			msg.exec_()
+		else:
+			settings.update(save_dir="/")
+			self.model.predict(self.listPath, show=False, save=True, conf=0.1)
 
 
 # Точка выполнения программы.
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    widget = Widget()
-    widget.show()
-    sys.exit(app.exec())
+	app = QApplication(sys.argv)
+	widget = Widget()
+	widget.show()
+	sys.exit(app.exec())
